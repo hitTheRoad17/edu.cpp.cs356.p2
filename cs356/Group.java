2c552d989275d16a1ab2014dde5586051dbd9bcf
hitTheRoad17 <yehanbo1216@gmail.com>
2017-07-18 23:44:06 -0700

    Add files via upload

--- cs356/Group.java
+++ cs356/Group.java
@@ -1,17 +1,38 @@
 package edu.cpp.cs356;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 public class Group extends Component {
 
-    Group(String str, Group g) {
-        setID(str);
-        parent = g;
-        children = new ArrayList<>();
+    public Group(String str) {
+        super(str);
     }
 
+    public List<Component> getComponents() {
+        List<Component> cl = new ArrayList<>();
+        Enumeration<DefaultMutableTreeNode> de = getNode().children();
+        while (de.hasMoreElements()) {
+            cl.add((Component) de.nextElement().getUserObject());
+        }
+        return cl;
+    }
+
+    @Override
     public void accept(Visitor v) {
         v.visit(this);
+        for (Component c : getComponents()) {
+            c.accept(v);
+        }
+    }
+
+    @Override
+    public void update(Observable o) {
+        for (Component c : getComponents()) {
+            c.update(o);
+        }
     }
 
 }
--- cs356/Tweet.java
+++ cs356/Tweet.java
@@ -2,28 +2,49 @@ package edu.cpp.cs356;
 
 public class Tweet {
 
+    private String id;
     private String tweetMsg;
 
-    Tweet(String str) {
-        tweetMsg = str;
-        isPositive();
+    private long timeCreated;
+
+    public Tweet(String str1, String str2) {
+        id = str1;
+        tweetMsg = str2;
+        timeCreated = System.currentTimeMillis();
     }
-    
+
+    public String getId() {
+        return id;
+    }
+
     public String getTweetMsg() {
         return tweetMsg;
+    }
+
+    public long getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(long timeCreated) {
+        this.timeCreated = timeCreated;
     }
 
     public boolean isPositive() {
         String[] positiveWords = {"nice", "great", "excellent",
             "awesome", "wonderful", "amazing", "positive"};
 
-        for (String word : positiveWords) {
-            if (tweetMsg.toLowerCase().contains(word)) {
+        for (String str : positiveWords) {
+            if (tweetMsg.toLowerCase().contains(str)) {
                 return true;
             }
         }
 
         return false;
     }
-    
+
+    @Override
+    public String toString() {
+        return "Tweet{" + "id=" + id + ", tweetMsg=" + tweetMsg + ", timeCreated=" + timeCreated + '}';
+    }
+
 }
