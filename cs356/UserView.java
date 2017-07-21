b5aadad4b8feaeea6cdb96ce3a5ed999c8b914cc
hitTheRoad17 <yehanbo1216@gmail.com>
2017-07-21 12:24:10 -0700

    Update UserView.java

--- cs356/UserView.java
+++ cs356/UserView.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.cpp.cs356;
 
 import javax.swing.DefaultListModel;
 
-/**
- *
- * @author hye
- */
 public class UserView extends javax.swing.JFrame {
 
     private User user;
